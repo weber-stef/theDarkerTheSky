454da61047fb1ffcde430f536c71dd5102d97e8e
Stefan Wurck <vitatech@gmx.de>
2019-03-25 16:08:23 +0100

    converted to class

--- dist/assets/js/bundle.js
+++ dist/assets/js/bundle.js
@@ -110,71 +110,118 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _scss_styles_scss__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_scss_styles_scss__WEBPACK_IMPORTED_MODULE_0__);
 /* harmony import */ var _img_logo_png__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @img/logo.png */ "./src/assets/img/logo.png");
 /* harmony import */ var _img_logo_png__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_img_logo_png__WEBPACK_IMPORTED_MODULE_1__);
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
 
 
-var weatherReport = document.querySelector(".weatherReport");
-var polaroidImage = document.querySelector(".polaroidImage");
-var myBrand = document.querySelector(".navbar-brand");
-myBrand.innerHTML = "Breaking News : Weather ahead!";
-window.addEventListener('click', function (ev) {
-  var elm = ev.target;
-  var selector = elm.getAttribute('data-target');
-  collapse(selector, 'toggle');
-}, false); // weather api
 
-var corsAnywhereProxy = "https://cors-anywhere.herokuapp.com/";
-var firstPartOfUrl = "https://api.darksky.net/forecast/";
-var myDarkSkyApiKey = "8e7c3b8124cb62d06ecaf994b8e8ea9f/"; // const myCoordinates = `52.520008,13.404954` // berlin
 
-var myCoordinates = "24,54";
-var url2Fetch = "".concat(corsAnywhereProxy).concat(firstPartOfUrl).concat(myDarkSkyApiKey).concat(myCoordinates);
-console.log(url2Fetch);
-var currentTemperature = "";
-var weAreIn = "";
-var temperatureHere = 0;
+var WeatherForecast =
+/*#__PURE__*/
+function () {
+  function WeatherForecast(lat, long) {
+    _classCallCheck(this, WeatherForecast);
 
-function gob() {
-  var feelGoodMode = "";
-  var shockOrNoShock = "";
-  var hotOrCold = "";
+    this.lat = lat;
+    this.long = long; // this.addMyEventListeners();
 
-  if (temperatureHere < 8) {
-    feelGoodMode = "kinda shitty..";
-    shockOrNoShock = "shocking";
-    hotOrCold = "freezing like crazy!!";
-  } else if (temperatureHere > 8 && temperatureHere < 20) {
-    feelGoodMode = "kinda ok";
-    shockOrNoShock = "";
-    hotOrCold = "feeling quite comfortable";
-  } else if (temperatureHere > 25 && temperatureHere < 60) {
-    feelGoodMode = "kinda too hot";
-    shockOrNoShock = "";
-    hotOrCold = "sweating like a crazy person";
-  }
+    this.render();
+  } // addMyEventListeners() {
+  //   // define querySel
+  //   // console.log("Adding Events");
+  //   const searchButton = document.querySelector("#searchButton");
+  //   const searchField = document.querySelector("#searchField");
+  //   // console.log(searchButton);
+  //   // console.log(earchField);
+  //   searchButton.addEventListener("click", e => {
+  //     console.log("click");
+  //     this.user = searchField.value;
+  //     this.render();
+  //   });
+  // }
+  // doneAt(time) {
+  //   let momentDate = moment(time);
+  //   let message = "";
+  //   // "2018-10-17T09:02:48Z"
+  //   return momentDate.format('DD.MM.YYYY');
+  // }
 
-  return [feelGoodMode, shockOrNoShock, hotOrCold];
-}
 
-function fToC(fahrenheit) {
-  var fTemp = fahrenheit;
-  var fToCel = (fTemp - 32) * 5 / 9;
-  return Math.floor(fToCel);
-}
+  _createClass(WeatherForecast, [{
+    key: "render",
+    value: function render() {
+      var _this = this;
 
-fetch(url2Fetch).then(function (response) {
-  return response.json();
-}).then(function (data) {
-  console.log(data);
-  temperatureHere = fToC(data.currently.temperature);
-  console.log(temperatureHere);
-  weAreIn = data.timezone.split('/')[1];
-  console.log(weAreIn);
-  console.log(gob(temperatureHere)[0]);
-  console.log(weAreIn);
-  polaroidImage.style.backgroundImage = "url(\"https://source.unsplash.com/random?".concat(weAreIn, "\")");
-  console.log(polaroidImage.style.backgroundImage);
-  weatherReport.innerHTML = "Hi there people, here in ".concat(weAreIn, " the weather is  ").concat(gob(temperatureHere)[0], ", it's ").concat(gob(temperatureHere)[1], " ").concat(temperatureHere, "\xB0 Celsius and I am ").concat(gob(temperatureHere)[2], "..");
-}); //     $(".reminderPicture")
+      // console.log("render");
+      // define search parameters
+      // weather api
+      var corsAnywhereProxy = "https://cors-anywhere.herokuapp.com/";
+      var firstPartOfUrl = "https://api.darksky.net/forecast/";
+      var myDarkSkyApiKey = "8e7c3b8124cb62d06ecaf994b8e8ea9f/"; // const myCoordinates = `52.520008,13.404954` // berlin
+
+      var url2Fetch = "".concat(corsAnywhereProxy).concat(firstPartOfUrl).concat(myDarkSkyApiKey).concat(this.lat, ",").concat(this.long);
+      console.log(url2Fetch);
+      var currentTemperature = "";
+      var weAreIn = "";
+      var temperatureHere = 0;
+      var weatherReport = document.querySelector(".weatherReport");
+      var polaroidImage = document.querySelector(".polaroidImage");
+      var myBrand = document.querySelector(".navbar-brand");
+      myBrand.innerHTML = "Breaking News : Weather ahead!";
+      fetch(url2Fetch).then(function (response) {
+        return response.json();
+      }).then(function (data) {
+        console.log(data);
+        temperatureHere = _this.fToC(data.currently.temperature);
+        console.log(temperatureHere);
+        weAreIn = data.timezone.split('/')[1];
+        console.log(weAreIn);
+        console.log(_this.gob(temperatureHere)[0]);
+        console.log(weAreIn);
+        polaroidImage.style.backgroundImage = "url(\"https://source.unsplash.com/random?".concat(weAreIn, "\")");
+        console.log(polaroidImage.style.backgroundImage);
+        weatherReport.innerHTML = "Hi there people, here in ".concat(weAreIn, " the weather is  ").concat(_this.gob(temperatureHere)[0], ", it's ").concat(_this.gob(temperatureHere)[1], " ").concat(temperatureHere, "\xB0 Celsius and I am ").concat(_this.gob(temperatureHere)[2], "..");
+      });
+    }
+  }, {
+    key: "gob",
+    value: function gob(temperatureHere) {
+      var feelGoodMode = "";
+      var shockOrNoShock = "";
+      var hotOrCold = "";
+
+      if (temperatureHere < 8) {
+        feelGoodMode = "kinda shitty..";
+        shockOrNoShock = "shocking";
+        hotOrCold = "freezing like crazy!!";
+      } else if (temperatureHere > 8 && temperatureHere < 20) {
+        feelGoodMode = "kinda ok";
+        shockOrNoShock = "";
+        hotOrCold = "feeling quite comfortable";
+      } else if (temperatureHere > 25 && temperatureHere < 60) {
+        feelGoodMode = "kinda too hot";
+        shockOrNoShock = "";
+        hotOrCold = "sweating like a crazy person";
+      }
+
+      return [feelGoodMode, shockOrNoShock, hotOrCold];
+    }
+  }, {
+    key: "fToC",
+    value: function fToC(fahrenheit) {
+      var fTemp = fahrenheit;
+      var fToCel = (fTemp - 32) * 5 / 9;
+      return Math.floor(fToCel);
+    }
+  }]);
+
+  return WeatherForecast;
+}();
+
+new WeatherForecast("-12.5", "18.5");
 
 /***/ }),
 
--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,68 +1,99 @@
 import "@scss/styles.scss";
 import logoImg from "@img/logo.png";
-const weatherReport = document.querySelector(".weatherReport");
-const polaroidImage = document.querySelector(".polaroidImage");
-const myBrand = document.querySelector(".navbar-brand");
-myBrand.innerHTML = "Breaking News : Weather ahead!"
 
-window.addEventListener('click', (ev) => {
-    const elm = ev.target;
-    const selector = elm.getAttribute('data-target');
-    collapse(selector, 'toggle');
-}, false);
+class WeatherForecast {
+  constructor(lat, long) {
+    this.lat = lat;
+    this.long = long;
+    // this.addMyEventListeners();
+    this.render();
+  }
 
-// weather api
-const corsAnywhereProxy = `https://cors-anywhere.herokuapp.com/`
-const firstPartOfUrl = `https://api.darksky.net/forecast/`;
-const myDarkSkyApiKey = `8e7c3b8124cb62d06ecaf994b8e8ea9f/`;
-// const myCoordinates = `52.520008,13.404954` // berlin
-const myCoordinates = `24,54`;
-let url2Fetch = `${corsAnywhereProxy}${firstPartOfUrl}${myDarkSkyApiKey}${myCoordinates}`
-console.log(url2Fetch);
-let currentTemperature = "";
-let weAreIn = "";
-let temperatureHere = 0;
+  // addMyEventListeners() {
+  //   // define querySel
+  //   // console.log("Adding Events");
+  //   const searchButton = document.querySelector("#searchButton");
+  //   const searchField = document.querySelector("#searchField");
+  //   // console.log(searchButton);
+  //   // console.log(earchField);
+  //   searchButton.addEventListener("click", e => {
+  //     console.log("click");
+  //     this.user = searchField.value;
 
-function gob() {
-    let feelGoodMode = "";
-    let shockOrNoShock = "";
-    let hotOrCold = "";
-    if (temperatureHere < 8) {
-        feelGoodMode = "kinda shitty..";
-        shockOrNoShock = "shocking";
-        hotOrCold = "freezing like crazy!!"
-    } else if (temperatureHere > 8 && temperatureHere < 20) {
-        feelGoodMode = "kinda ok";
-        shockOrNoShock = "";
-        hotOrCold = "feeling quite comfortable";
-    } else if (temperatureHere > 25 && temperatureHere < 60) {
-        feelGoodMode = "kinda too hot";
-        shockOrNoShock = "";
-        hotOrCold = "sweating like a crazy person";
-    }
-    return [feelGoodMode, shockOrNoShock, hotOrCold];
-}
+  //     this.render();
+  //   });
 
-function fToC(fahrenheit) {
-    var fTemp = fahrenheit;
-    var fToCel = (fTemp - 32) * 5 / 9;
-    return Math.floor(fToCel);
-}
+  // }
+  // doneAt(time) {
+  //   let momentDate = moment(time);
+  //   let message = "";
+  //   // "2018-10-17T09:02:48Z"
 
-fetch(url2Fetch)
-    .then(response => response.json())
-    .then(data => {
+  //   return momentDate.format('DD.MM.YYYY');
+  // }
+
+  render() {
+    // console.log("render");
+    // define search parameters
+    // weather api
+    const corsAnywhereProxy = `https://cors-anywhere.herokuapp.com/`
+    const firstPartOfUrl = `https://api.darksky.net/forecast/`;
+    const myDarkSkyApiKey = `8e7c3b8124cb62d06ecaf994b8e8ea9f/`;
+    // const myCoordinates = `52.520008,13.404954` // berlin
+    let url2Fetch = `${corsAnywhereProxy}${firstPartOfUrl}${myDarkSkyApiKey}${this.lat},${this.long}`
+    console.log(url2Fetch);
+    let currentTemperature = "";
+    let weAreIn = "";
+    let temperatureHere = 0;
+    const weatherReport = document.querySelector(".weatherReport");
+    const polaroidImage = document.querySelector(".polaroidImage");
+    const myBrand = document.querySelector(".navbar-brand");
+    myBrand.innerHTML = "Breaking News : Weather ahead!"
+    fetch(url2Fetch)
+      .then(response => response.json())
+      .then(data => {
         console.log(data);
-        temperatureHere = fToC(data.currently.temperature);
+        temperatureHere = this.fToC(data.currently.temperature);
         console.log(temperatureHere);
         weAreIn = (data.timezone.split('/')[1]);
         console.log(weAreIn);
-        console.log(gob(temperatureHere)[0]);
+        console.log(this.gob(temperatureHere)[0]);
         console.log(weAreIn);
         polaroidImage.style.backgroundImage = `url("https://source.unsplash.com/random?${weAreIn}")`;
         console.log(polaroidImage.style.backgroundImage);
 
-        weatherReport.innerHTML = `Hi there people, here in ${weAreIn} the weather is  ${gob(temperatureHere)[0]}, it's ${gob(temperatureHere)[1]} ${temperatureHere}° Celsius and I am ${gob(temperatureHere)[2]}..`
-    });
+        weatherReport.innerHTML = `Hi there people, here in ${weAreIn} the weather is  ${this.gob(temperatureHere)[0]}, it's ${this.gob(temperatureHere)[1]} ${temperatureHere}° Celsius and I am ${this.gob(temperatureHere)[2]}..`
+      });
+  }
 
-//     $(".reminderPicture")
+  gob(temperatureHere) {
+    let feelGoodMode = "";
+    let shockOrNoShock = "";
+    let hotOrCold = "";
+    if (temperatureHere < 8) {
+      feelGoodMode = "kinda shitty..";
+      shockOrNoShock = "shocking";
+      hotOrCold = "freezing like crazy!!"
+    } else if (temperatureHere > 8 && temperatureHere < 20) {
+      feelGoodMode = "kinda ok";
+      shockOrNoShock = "";
+      hotOrCold = "feeling quite comfortable";
+    } else if (temperatureHere > 25 && temperatureHere < 60) {
+      feelGoodMode = "kinda too hot";
+      shockOrNoShock = "";
+      hotOrCold = "sweating like a crazy person";
+    }
+    return [feelGoodMode, shockOrNoShock, hotOrCold];
+  }
+
+  fToC(fahrenheit) {
+    var fTemp = fahrenheit;
+    var fToCel = (fTemp - 32) * 5 / 9;
+    return Math.floor(fToCel);
+  }
+
+
+}
+
+
+new WeatherForecast("-12.5", "18.5");
